819d65e9575246dd0829cd33ffab89732b4b820b
pablozoani <pablozoani@hotmail.com>
2020-06-04 01:11:54 -0300

    Initial commit

---
+++ src/main/java/com/pablozoani/pzpetclinic/PzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.pablozoani.pzpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PzPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PzPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablozoani/pzpetclinic/PzPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablozoani.pzpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PzPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
