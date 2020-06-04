9543fc2bc53f926004955fe09216adb35700ba27
pablozoani <pablozoani@hotmail.com>
2020-06-04 19:29:37 -0300

    Added CrudService Interface plus Refactoring. Closes #10.

--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/OwnerService.java
@@ -2,12 +2,7 @@ package com.pablozoani.pzpetclinic.service;
 
 import com.pablozoani.pzpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/PetService.java
@@ -2,11 +2,5 @@ package com.pablozoani.pzpetclinic.service;
 
 import com.pablozoani.pzpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
