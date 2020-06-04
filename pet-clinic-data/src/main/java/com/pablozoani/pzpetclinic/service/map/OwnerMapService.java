3576a078b8a8ad04af23358fada4355c79d5d68e
pablozoani <pablozoani@hotmail.com>
2020-06-04 20:09:37 -0300

    Implementing Map Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/OwnerMapService.java
@@ -0,0 +1,34 @@
+package com.pablozoani.pzpetclinic.service.map;
+
+import com.pablozoani.pzpetclinic.model.Owner;
+import com.pablozoani.pzpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
