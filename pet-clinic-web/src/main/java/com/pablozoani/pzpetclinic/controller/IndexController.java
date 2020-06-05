5e9803ca1829dd68ef03de9e80e69f8faaa3eb1b
pablozoani <pablozoani@hotmail.com>
2020-06-04 22:33:02 -0300

    Adding owner index and controller. Closes #12.

--- pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/controller/IndexController.java
@@ -2,11 +2,13 @@ package com.pablozoani.pzpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({"/"})
 @Controller
 public class IndexController {
 
-    @GetMapping({"/"})
+    @GetMapping({""})
     public String index() {
         return "index";
     }
