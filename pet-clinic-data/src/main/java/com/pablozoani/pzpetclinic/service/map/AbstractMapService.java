1d7a1d787c4d5ed235a59afed67d33d29811c9f5
pablozoani <pablozoani@hotmail.com>
2020-06-05 07:16:40 -0300

    Now the Map<> implementation generates ID values. Closes #23.

--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.pablozoani.pzpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.pablozoani.pzpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T t) {
-        map.put(id, t);
+    T save(T t) {
+        if (t != null) {
+            if (t.getId() == null) {
+                t.setId(getNextId());
+            }
+
+            map.put(t.getId(), t);
+        } else {
+            throw new RuntimeException("t is null");
+        }
+
         return t;
     }
 
@@ -28,5 +36,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T t) {
         map.entrySet().removeIf(e -> e.getValue().equals(t));
+    }
+
+    private Long getNextId() {
+        Long output = null;
+        try {
+            output = Collections.max(map.keySet()) + 1L;
+        } catch (NoSuchElementException e) {
+            output = 1L;
+        }
+        return output;
     }
 }
--- pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
@@ -23,25 +23,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner o1 = new Owner();
-        o1.setId(1L);
         o1.setFirstName("Peter");
         o1.setLastName("Parker");
         ownerService.save(o1);
 
         Owner o2 = new Owner();
-        o2.setId(2L);
         o2.setFirstName("Bruce");
         o2.setLastName("Wayne");
         ownerService.save(o2);
 
         Vet v1 = new Vet();
-        v1.setId(1L);
         v1.setFirstName("Tony");
         v1.setLastName("Stark");
         vetService.save(v1);
 
         Vet v2 = new Vet();
-        v2.setId(2L);
         v2.setFirstName("Frodo");
         v2.setLastName("Baggins");
         vetService.save(v2);
