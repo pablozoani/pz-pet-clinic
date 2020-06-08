e462b229c7a70cada832f0149c0c440586f9370d
pablozoani <pablozoani@hotmail.com>
2020-06-08 07:54:24 -0300

    Adding PetTypes in with Bootstrap (start up). closes #32

--- pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.pablozoani.pzpetclinic.bootstrap;
 
 import com.pablozoani.pzpetclinic.model.Owner;
+import com.pablozoani.pzpetclinic.model.PetType;
 import com.pablozoani.pzpetclinic.model.Vet;
 import com.pablozoani.pzpetclinic.service.OwnerService;
+import com.pablozoani.pzpetclinic.service.PetTypeService;
 import com.pablozoani.pzpetclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,29 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,
+                      PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("bootstrap data started");
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
         Owner o1 = new Owner();
         o1.setFirstName("Peter");
         o1.setLastName("Parker");
@@ -41,5 +57,7 @@ public class DataLoader implements CommandLineRunner {
         v2.setFirstName("Frodo");
         v2.setLastName("Baggins");
         vetService.save(v2);
+
+        System.out.println("bootstrap data ended");
     }
 }
