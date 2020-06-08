20c976a185e7489d80e0f05273d300a66663080d
pablozoani <pablozoani@hotmail.com>
2020-06-08 07:38:36 -0300

    Creating PetType Map Service. closes #29

---
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.pablozoani.pzpetclinic.service;
+
+import com.pablozoani.pzpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.pablozoani.pzpetclinic.service.map;
+
+import com.pablozoani.pzpetclinic.model.PetType;
+import com.pablozoani.pzpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public void delete(PetType t) {
+        super.delete(t);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO
+    }
+}
