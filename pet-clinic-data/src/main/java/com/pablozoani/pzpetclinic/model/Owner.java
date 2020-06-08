53458c4bb747d97b897659ab912c24a21f9521af
pablozoani <pablozoani@hotmail.com>
2020-06-08 06:54:34 -0300

    adding contact info to the Owner class. closes #28

--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Owner.java
@@ -4,7 +4,37 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Owner setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Owner setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public Owner setTelephone(String telephone) {
+        this.telephone = telephone;
+        return this;
+    }
 
     public Set<Pet> getPets() {
         return pets;
