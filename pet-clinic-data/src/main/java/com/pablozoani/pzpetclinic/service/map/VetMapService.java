802ff16f5271c73071a9b267581146c5cf5c5cd1
pablozoani <pablozoani@hotmail.com>
2020-06-08 15:19:09 -0300

    Creating Specialities, add to Vets on Startup (Bootstrap). closes #34

--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/VetMapService.java
@@ -2,12 +2,20 @@ package com.pablozoani.pzpetclinic.service.map;
 
 import com.pablozoani.pzpetclinic.model.Vet;
 import com.pablozoani.pzpetclinic.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyMapService specialtyMapService;
+
+    @Autowired
+    public VetMapService(SpecialtyMapService specialtyMapService) {
+        this.specialtyMapService = specialtyMapService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,17 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (!vet.getSpecialties().isEmpty()) {
+            vet.getSpecialties().forEach(specialty -> {
+                if (specialty != null) {
+                    if (specialty.getId() != null) {
+                        specialtyMapService.save(specialty);
+                    }
+                } else {
+                    throw new RuntimeException("null specialty");
+                }
+            });
+        }
         return super.save(vet);
     }
 
--- pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/pablozoani/pzpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.pablozoani.pzpetclinic.bootstrap;
 
-import com.pablozoani.pzpetclinic.model.Owner;
-import com.pablozoani.pzpetclinic.model.Pet;
-import com.pablozoani.pzpetclinic.model.PetType;
-import com.pablozoani.pzpetclinic.model.Vet;
+import com.pablozoani.pzpetclinic.model.*;
 import com.pablozoani.pzpetclinic.service.OwnerService;
 import com.pablozoani.pzpetclinic.service.PetTypeService;
+import com.pablozoani.pzpetclinic.service.SpecialtyService;
 import com.pablozoani.pzpetclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -20,23 +18,34 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
     @Autowired
     public DataLoader(OwnerService ownerService, VetService vetService,
-                      PetTypeService petTypeService) {
+                      PetTypeService petTypeService,
+                      SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("bootstrap data started");
 
+        int petCount = petTypeService.findAll().size();
+        if (petCount == 0) {
+            loadData();
+        }
+
+        System.out.println("bootstrap data ended");
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogType = petTypeService.save(dog);
-
 
         PetType cat = new PetType();
         cat.setName("Cat");
@@ -70,16 +79,29 @@ public class DataLoader implements CommandLineRunner {
         o2.getPets().add(brucesCat);
         ownerService.save(o2);
 
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        specialtyService.save(radiology);
+
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("Dentistry");
+        specialtyService.save(dentistry);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        specialtyService.save(surgery);
+
         Vet v1 = new Vet();
         v1.setFirstName("Tony");
         v1.setLastName("Stark");
+        v1.getSpecialties().add(dentistry);
+        v1.getSpecialties().add(radiology);
         vetService.save(v1);
 
         Vet v2 = new Vet();
         v2.setFirstName("Frodo");
         v2.setLastName("Baggins");
+        v2.getSpecialties().add(surgery);
         vetService.save(v2);
-
-        System.out.println("bootstrap data ended");
     }
 }
