57eba14ca9b068cec7cc99d1cf97b3a473920e71
pablozoani <pablozoani@hotmail.com>
2020-06-08 14:45:18 -0300

    Creating Specialty Map Service Impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.pablozoani.pzpetclinic.service;
+
+import com.pablozoani.pzpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/service/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package com.pablozoani.pzpetclinic.service.map;
+
+import com.pablozoani.pzpetclinic.model.Specialty;
+import com.pablozoani.pzpetclinic.service.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
