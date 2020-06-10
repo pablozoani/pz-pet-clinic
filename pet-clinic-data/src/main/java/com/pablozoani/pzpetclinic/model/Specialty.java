60ab4a2528e09d436baee77fe4ef08938f2d7c3e
pablozoani <pablozoani@hotmail.com>
2020-06-10 14:13:18 -0300

    Converted Vets to JPA Entities. closes #37

--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Specialty.java
@@ -1,7 +1,12 @@
 package com.pablozoani.pzpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/pablozoani/pzpetclinic/model/Visit.java
@@ -1,11 +1,22 @@
 package com.pablozoani.pzpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import java.time.LocalDate;
 
+@Entity
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @OneToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
